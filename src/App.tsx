9154512e8688d9b749f055dbc7618c73949367b4
João Vitor <joaovitorfbarbosa@gmail.com>
2025-02-14 12:02:48 -0300

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/" element={<Home/>} />
     </Routes>
   )
 }
